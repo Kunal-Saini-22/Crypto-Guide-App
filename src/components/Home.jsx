9ee5171cb41331636da6c789c8c1450cc6e5ef58
Kunal Saini <kunalsaini22112@gmail.com>
2023-01-30 18:02:11 +0530

    home page styling added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import btcSrc from "../assets/btclogo.png";
+import { motion } from "framer-motion";
+import { Box, Image, Text } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box bgColor={"blackAlpha.800"} w={"full"} h={"85vh"}>
+      <motion.div
+        style={{
+          height: "80vh",
+        }}
+        animate={{
+          translateY: "20px",
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+        <Image
+          w={"full"}
+          h={"96"}
+          objectFit={"contain"}
+          src={btcSrc}
+        />
+      </motion.div>
+
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+        mt={"-20"}
+      >
+        Crypto App
+      </Text>
+    </Box>
   )
 }
 
